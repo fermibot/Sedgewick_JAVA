c701467ef8ac23eb29240cbce1a4cf99f93e8288
fermibot <ashwinuoh@gmail.com>
2019-08-26 21:02:29 -0500

    solved some more examples from 1.2

---
+++ src/com/sedgewick/program_1_2_13_e.java
@@ -0,0 +1,7 @@
+package com.sedgewick;
+
+public class program_1_2_13_e {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(Math.sqrt(2), 2) == 2);
+    }
+}
---
+++ src/program_1_2_22_e.java
@@ -0,0 +1,7 @@
+public class program_1_2_22_e {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Math.sin(0.01 * i) + Math.sin(0.01 * i));
+        }
+    }
+}

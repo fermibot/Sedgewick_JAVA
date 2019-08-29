4c0ba59b5fd5c8d25bf809f535e6c028ef4b9f3d
fermibot <ashwinuoh@gmail.com>
2019-08-29 06:35:34 -0500

    Added Rational.JAVA

---
+++ src/com/sedgewick/Rational.java
@@ -0,0 +1,36 @@
+package com.sedgewick;
+
+public class Rational {
+    private float n;
+
+    private Rational(float rational_in) {
+        n = rational_in;
+    }
+
+    private float Plus(Rational a) {
+        return n + a.n;
+    }
+
+    private float Minus(Rational a) {
+        return n - a.n;
+    }
+
+    private float Times(Rational a) {
+        return n * a.n;
+    }
+
+    private float Divide(Rational a) {
+        return n / a.n;
+    }
+
+    private boolean Equals(Rational a) {
+        return n == a.n;
+    }
+
+    public static void main(String[] args) {
+        Rational a = new Rational(10);
+        Rational b = new Rational(11);
+        System.out.println(a.Plus(a));
+        System.out.println(a.Plus(b));
+    }
+}
--- src/com/sedgewick/program_1_2_25_e.java
+++ src/com/sedgewick/program_1_2_25_e.java
@@ -4,7 +4,7 @@ import static com.sedgewick.custom_functions.wind_chill;
 
 public class program_1_2_25_e {
     public static void main(String[] args) {
-        for (double temperature = -50; temperature < 51; temperature = temperature + 5) {
+        for (double temperature = -50; temperature < 51; temperature = temperature + 2) {
             for (int velocity = 3; velocity < 121; velocity = velocity + 7) {
                 System.out.println("Temperature::" + temperature + "||Velocity::" + velocity +
                         "||WindChill::" + wind_chill(temperature, velocity));

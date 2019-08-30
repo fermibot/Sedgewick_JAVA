e3ad9edfa51f39c132e3669a7a3ea87b14b3c151
fermibot <ashwinuoh@gmail.com>
2019-08-30 18:20:19 -0500

    added Times and Divide to the Complex function

--- src/com/sedgewick/math_objects/Complex.java
+++ src/com/sedgewick/math_objects/Complex.java
@@ -1,5 +1,8 @@
 package com.sedgewick.math_objects;
 
+import static com.sedgewick.math_objects.custom_functions.Norm;
+import static java.lang.Math.*;
+
 public class Complex {
     private double real;
     private double complex;
@@ -29,33 +32,51 @@ public class Complex {
         return new Complex(real - b.real, complex - b.complex);
     }
 
-    public double Arg(Complex a) {
-        return Math.atan2(a.complex, a.real);
+    public double Arg() {
+        return atan2(complex, real);
     }
 
-
-    public double Abs(Complex a) {
-        return Math.sqrt(Math.pow(a.real, 2 + Math.pow(a.complex, 2)));
-    }
 
     public double Abs() {
-        return Math.sqrt(Math.pow(real, 2) + Math.pow(complex, 2));
+        return Norm(real, complex);
     }
 
+    public Complex Times(Complex a) {
+        double abs_product = Abs() * a.Abs();
+        double arg_sum = Arg() + a.Arg();
+        return new Complex(abs_product * cos(arg_sum), abs_product * sin(arg_sum));
+    }
+
+    public Complex Divide(Complex a) {
+        double abs_divide = Abs() / a.Abs();
+        double arg_diff = Arg() - a.Arg();
+        return new Complex(abs_divide * cos(arg_diff), abs_divide * sin(arg_diff));
+    }
+
+    public void printReal() {
+        System.out.println("Real: " + real);
+    }
+
+    public void printComplex() {
+        System.out.println("Complex: " + complex);
+    }
 
     private Complex Times(double a) {
         return new Complex(a * real, a * complex);
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            Complex a = new Complex(Math.random(), Math.random());
-            Complex b = new Complex(Math.random(), Math.random());
-            System.out.print("Real: " + a.Plus(b).real);
-            System.out.print(" Complex: ");
-            System.out.println(a.Plus(b).complex);
-            System.out.println(a.Abs());
-            System.out.println();
-        }
+        Complex a = new Complex(1, 1);
+        Complex b = new Complex(1, 1);
+        Complex plus = a.Plus(b);
+        plus.printReal();
+        plus.printComplex();
+
+        System.out.println(a.Abs());
+        System.out.println(a.Arg());
+        System.out.println(a.Times(b).real);
+        System.out.println(a.Times(b).complex);
+        System.out.println(a.Divide(b).real);
+        System.out.println(a.Divide(b).complex);
     }
 }
--- src/com/sedgewick/math_objects/custom_functions.java
+++ src/com/sedgewick/math_objects/custom_functions.java
@@ -1,5 +1,6 @@
 package com.sedgewick.math_objects;
 
+import static java.lang.Math.sqrt;
 import static java.lang.Math.exp;
 
 public class custom_functions {
@@ -17,6 +18,25 @@ public class custom_functions {
 
     public static double wind_chill(double temperature, double wind_velocity) {
         return 35.74 + (0.6215 * temperature) + (((0.4275 * temperature) - 35.75) * Math.pow(wind_velocity, 0.16));
+    }
+
+    public static double Norm(double[] vector) {
+        double squared_sum = 0;
+        for (double item :
+                vector) {
+            squared_sum += item * item;
+        }
+        return sqrt(squared_sum);
+    }
+
+    public static double Norm(double x1, double x2) {
+        double[] vector = {x1, x2};
+        return Norm(vector);
+    }
+
+    public static double Norm(double x1, double x2, double x3) {
+        double[] vector = {x1, x2, x3};
+        return Norm(vector);
     }
 
 }

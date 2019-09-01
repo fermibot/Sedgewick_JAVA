c27423676cbfe87925ae280fa25569a9bdb66018
fermibot <ashwinuoh@gmail.com>
2019-08-31 21:43:21 -0500

    completed 1_2_30_3.java

--- src/sedgewick_java/program_1_2_30_e.java
+++ src/sedgewick_java/program_1_2_30_e.java
@@ -5,7 +5,7 @@ import static com.sedgewick.math_objects.Statistics.Mean;
 
 public class program_1_2_30_e {
     public static void main(String[] args) {
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(Mean(RandomReal(0, 1,5)));
         }
     }

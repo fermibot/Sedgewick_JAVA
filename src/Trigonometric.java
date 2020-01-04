1dbc69cfeeb6f1609d5730e23e6d5b2e02599ded
fermibot <ashwinuoh@gmail.com>
2020-01-04 12:35:53 -0600

    Trigonometric.java : small fixes for the factorial function to deal with NaN. The issues with NaN persist just as with the previous version, but there is better handling.

--- src/Trigonometric.java
+++ src/Trigonometric.java
@@ -3,27 +3,26 @@ import static java.lang.Math.pow;
 
 public class Trigonometric {
 
+    int precision;
+
     public Trigonometric(int precisionIn) {
         precision = precisionIn;
     }
 
-    private int precision = 50;
-
-    private int Factorial(int x) {
-        int factorial = 0;
+    private double Factorial(double x) {
+        double factorial = 0;
         if (x == 0) return 1;
         if (x == 1) return 1;
         if (x > 1) factorial = x * Factorial(x - 1);
         return factorial;
     }
 
-
     private double sine_cos(double x, int one_or_zero) {
         double _sine_cos = 0;
 
         for (int i = 0; i < precision; i++) {
             int expFact = 2 * i + one_or_zero;
-            _sine_cos += pow((-1), i) * pow(x, expFact) / Factorial(expFact);
+            _sine_cos += pow(-1, i) * pow(x, expFact) / Factorial(expFact);
         }
         return _sine_cos;
     }
@@ -38,7 +37,9 @@ public class Trigonometric {
     }
 
     public static void main(String[] args) {
-        Trigonometric trigonometric = new Trigonometric(50);
+        Trigonometric trigonometric = new Trigonometric(120);
         System.out.println(trigonometric.Factorial(5));
+        System.out.println(trigonometric.Sin(10));
+        System.out.println(trigonometric.Cos(10));
     }
 }
